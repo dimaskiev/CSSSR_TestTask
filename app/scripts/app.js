3597445e7ced22d8b4e302993978469a9d370244
Dmytro Romanchuk <manager.ua@gmail.com>
2017-08-10 23:25:59 +0300

    finish checkbox, text (footer)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,7 @@ import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 require('../blocks/futures/futures.js');
 require('../blocks/about__checkbox/about__checkbox.js');
+require('../blocks/skills/skills.js');
 
 
 $(() => {
